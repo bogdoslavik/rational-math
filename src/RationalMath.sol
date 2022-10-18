f5c5437ee1875ddfa7217758a20fda2bb683d721
Viacheslav Bogdanov <viacheslav.v.bogdanov@gmail.com>
2022-10-18 16:42:34 +0700

    rename

--- src/RationalMath.sol
+++ src/RationalMath.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 
 /// @author bogdoslav
 
-struct Rational {
+struct rational {
     //bool positive;
     uint numerator;
     uint denominator;
@@ -12,91 +12,91 @@ struct Rational {
 
 error ZeroDenominator();
 
-library RationalMath {
+library Rational {
 
     // RATIONAL FROM
 
-    function from(uint numerator, uint denominator) internal pure returns (Rational memory result) {
+    function from(uint numerator, uint denominator) internal pure returns (rational memory result) {
         if (denominator == 0) revert ZeroDenominator();
         result.numerator = numerator;
         result.denominator = denominator;
     }
 
-    function from(uint integer) internal pure returns (Rational memory result) {
+    function from(uint integer) internal pure returns (rational memory result) {
         result.numerator = integer;
         result.denominator = 1;
     }
 
     /// @notice creates new memory variable and copies value
-    function from(Rational memory a) internal pure returns (Rational memory result) {
+    function from(rational memory a) internal pure returns (rational memory result) {
         result.numerator = a.numerator;
         result.denominator = a.denominator;
     }
 
-    function fromFixed(uint integer, uint8 decimals) internal pure returns (Rational memory result) {
+    function fromFixed(uint integer, uint8 decimals) internal pure returns (rational memory result) {
         result.numerator = integer;
         result.denominator = 10**decimals;
     }
 
     // CONSTANTS
 
-    function zero() internal pure returns (Rational memory result) {
+    function zero() internal pure returns (rational memory result) {
         result.numerator = 0;
         result.denominator = 1;
     }
 
-    function one() internal pure returns (Rational memory result) {
+    function one() internal pure returns (rational memory result) {
         result.numerator = 1;
         result.denominator = 1;
     }
 
     // MATH
 
-    function _add(Rational memory a, Rational memory b) internal pure returns (Rational memory result) {
+    function _add(rational memory a, rational memory b) internal pure returns (rational memory result) {
         result.numerator = a.numerator * b.denominator + b.numerator * a.denominator;
         result.denominator = a.denominator * b.denominator;
     }
 
-    function _sub(Rational memory a, Rational memory b) internal pure returns (Rational memory result) {
+    function _sub(rational memory a, rational memory b) internal pure returns (rational memory result) {
         result.numerator = a.numerator * b.denominator - b.numerator * a.denominator;
         result.denominator = a.denominator * b.denominator;
     }
 
-    function _mul(Rational memory a, Rational memory b) internal pure returns (Rational memory result) {
+    function _mul(rational memory a, rational memory b) internal pure returns (rational memory result) {
         result.numerator = a.numerator * b.numerator;
         result.denominator = a.denominator * b.denominator;
     }
 
-    function _div(Rational memory a, Rational memory b) internal pure returns (Rational memory result) {
+    function _div(rational memory a, rational memory b) internal pure returns (rational memory result) {
         result.numerator = a.numerator * b.denominator;
         result.denominator = a.denominator * b.numerator;
     }
 
     // REDUCED MATH
 
-    function add(Rational memory a, Rational memory b) internal pure returns (Rational memory result) {
+    function add(rational memory a, rational memory b) internal pure returns (rational memory result) {
         result = _add(a, b);
         reduceIt(result);
     }
 
-    function sub(Rational memory a, Rational memory b) internal pure returns (Rational memory result) {
+    function sub(rational memory a, rational memory b) internal pure returns (rational memory result) {
         result = _sub(a, b);
         reduceIt(result);
     }
 
-    function mul(Rational memory a, Rational memory b) internal pure returns (Rational memory result) {
+    function mul(rational memory a, rational memory b) internal pure returns (rational memory result) {
         result = _mul(a, b);
         reduceIt(result);
     }
 
-    function div(Rational memory a, Rational memory b) internal pure returns (Rational memory result) {
+    function div(rational memory a, rational memory b) internal pure returns (rational memory result) {
         result = _div(a, b);
         reduceIt(result);
     }
 
     // REDUCING
 
-    function reduceIt(Rational memory r) internal pure {
+    function reduceIt(rational memory r) internal pure {
         if (r.numerator == 0) {
             r.denominator = 1;
         } if (r.numerator == r.denominator) {
@@ -109,7 +109,7 @@ library RationalMath {
         }
     }
 
-    function reduce(Rational memory r) internal pure returns (Rational memory result) {
+    function reduce(rational memory r) internal pure returns (rational memory result) {
         result.numerator = r.numerator;
         result.denominator = r.denominator;
         reduceIt(result);
@@ -128,47 +128,47 @@ library RationalMath {
 
     // COMPARISON
 
-    function _eq(Rational memory a, Rational memory b) internal pure returns (bool) {
+    function _eq(rational memory a, rational memory b) internal pure returns (bool) {
         return (a.numerator * b.denominator == b.numerator * a.denominator);
     }
 
-    function _gt(Rational memory a, Rational memory b) internal pure returns (bool) {
+    function _gt(rational memory a, rational memory b) internal pure returns (bool) {
         return (a.numerator * b.denominator > b.numerator * a.denominator);
     }
 
-    function _lt(Rational memory a, Rational memory b) internal pure returns (bool) {
+    function _lt(rational memory a, rational memory b) internal pure returns (bool) {
         return (a.numerator * b.denominator < b.numerator * a.denominator);
     }
 
-    function _gte(Rational memory a, Rational memory b) internal pure returns (bool) {
+    function _gte(rational memory a, rational memory b) internal pure returns (bool) {
         return (a.numerator * b.denominator >= b.numerator * a.denominator);
     }
 
-    function _lte(Rational memory a, Rational memory b) internal pure returns (bool) {
+    function _lte(rational memory a, rational memory b) internal pure returns (bool) {
         return (a.numerator * b.denominator <= b.numerator * a.denominator);
     }
 
     // TO
 
-    function toInt(Rational memory r) internal pure returns (uint) {
+    function toInt(rational memory r) internal pure returns (uint) {
         return r.numerator / r.denominator;
     }
 
-    function toBase(Rational memory r, uint base) internal pure returns (uint) {
+    function toBase(rational memory r, uint base) internal pure returns (uint) {
         return r.numerator * base / r.denominator;
     }
 
-    function toFixed(Rational memory r, uint8 decimals) internal pure returns (uint) {
+    function toFixed(rational memory r, uint8 decimals) internal pure returns (uint) {
         return r.numerator * 10**decimals / r.denominator;
     }
 
     // DOWNGRADE
 
-    /// @return r most significant bit (1-based). For 0 returns 0
+    /// @return r significant bits (for 0 returns 0)
     /// Idea got from Mikhail Vladimirov's article  from https://medium.com/coinmonks/math-in-solidity-part-3-percents-and-proportions-4db014e080b1
-    function mostSignificantBit(uint x) public pure returns (uint r) {
+    function significantBits(uint x) public pure returns (uint r) {
         if (x == 0) return 0;
-        r = 0;
+        r = 1;
         if (x >= 2**128) { x >>= 128; r += 128; }
         if (x >= 2**64) { x >>= 64; r += 64; }
         if (x >= 2**32) { x >>= 32; r += 32; }
@@ -178,4 +178,5 @@ library RationalMath {
         if (x >= 2**2) { x >>= 2; r += 2; }
         if (x >= 2**1) { x >>= 1; r += 1; }
     }
+
 }
--- test/RationalMath.t.sol
+++ test/RationalMath.t.sol
@@ -5,8 +5,8 @@ import "forge-std/Test.sol";
 import "../src/RationalMath.sol";
 import "forge-std/console.sol";
 
-contract RationalMathTest is Test {
-    using RationalMath for Rational;
+contract RationalTest is Test {
+    using Rational for rational;
 
     function setUp() public {
     }
@@ -15,7 +15,7 @@ contract RationalMathTest is Test {
 
     function testFromFraction(uint n, uint d) public {
         if (d == 0) d = 1;
-        Rational memory r = RationalMath.from(n, d);
+        rational memory r = Rational.from(n, d);
         assertEq(r.numerator, n);
         assertEq(r.denominator, d);
     }
@@ -23,11 +23,11 @@ contract RationalMathTest is Test {
     function testFromFractionDivByZero(uint n) public {
         uint d = 0;
         vm.expectRevert(ZeroDenominator.selector);
-        RationalMath.from(n, d);
+        Rational.from(n, d);
     }
 
     function testFromUint(uint integer) public {
-        Rational memory r = RationalMath.from(integer);
+        rational memory r = Rational.from(integer);
         assertEq(r.numerator, integer);
         assertEq(r.denominator, 1);
     }
@@ -35,8 +35,8 @@ contract RationalMathTest is Test {
     function testFromRational(uint n, uint d) public {
         if (n == 0) n = 1;
         if (d == 0) d = 1;
-        Rational memory r = RationalMath.from(n, d);
-        Rational memory rr = RationalMath.from(r);
+        rational memory r = Rational.from(n, d);
+        rational memory rr = Rational.from(r);
         assertEq(rr.numerator, r.numerator);
         assertEq(rr.denominator, r.denominator);
 
@@ -49,7 +49,7 @@ contract RationalMathTest is Test {
 
     function testFromFixed(uint n, uint8 decimals) public {
         if (decimals > 77) decimals = 77;
-        Rational memory r = RationalMath.fromFixed(n, decimals);
+        rational memory r = Rational.fromFixed(n, decimals);
         assertEq(r.numerator, n);
         assertEq(r.denominator, 10**decimals);
     }
@@ -57,13 +57,13 @@ contract RationalMathTest is Test {
     // Constants
 
     function testZero() public {
-        Rational memory r = RationalMath.zero();
+        rational memory r = Rational.zero();
         assertEq(r.numerator, 0);
         assertEq(r.denominator, 1);
     }
 
     function testOne() public {
-        Rational memory r = RationalMath.one();
+        rational memory r = Rational.one();
         assertEq(r.numerator, 1);
         assertEq(r.denominator, 1);
     }
@@ -73,35 +73,35 @@ contract RationalMathTest is Test {
     function testCommonDivisor(uint n, uint d) public {
         if (n == 0) n = 1;
         if (d == 0) d = 1;
-        uint divisor = RationalMath.commonDivisor(n, d);
+        uint divisor = Rational.commonDivisor(n, d);
         assertEq(n % divisor, 0);
         assertEq(d % divisor, 0);
     }
 
     function testCommonDivisor_0_1() public {
-        uint divisor = RationalMath.commonDivisor(0, 1);
+        uint divisor = Rational.commonDivisor(0, 1);
         assertEq(divisor, 1);
     }
 
     function testCommonDivisor_1_1() public {
-        uint divisor = RationalMath.commonDivisor(1, 1);
+        uint divisor = Rational.commonDivisor(1, 1);
         assertEq(divisor, 1);
     }
 
     function testCommonDivisor_2_4() public {
-        uint divisor = RationalMath.commonDivisor(2, 4);
+        uint divisor = Rational.commonDivisor(2, 4);
         assertEq(divisor, 2);
     }
 
     function testCommonDivisor_140_96() public {
-        uint divisor = RationalMath.commonDivisor(140, 96);
+        uint divisor = Rational.commonDivisor(140, 96);
         assertEq(divisor, 4);
     }
 
     function testCommonDivisor_prime() public {
         uint n = 19999897;
         uint d = 1002299;
-        uint divisor = RationalMath.commonDivisor(n, d);
+        uint divisor = Rational.commonDivisor(n, d);
         assertEq(divisor, 1);
     }
 
@@ -109,7 +109,7 @@ contract RationalMathTest is Test {
         if (c == 0) c = 1;
         uint n = 19995023;
         uint d = 20999941;
-        uint divisor = RationalMath.commonDivisor(n *c, d *c);
+        uint divisor = Rational.commonDivisor(n *c, d *c);
         assertEq(divisor, c);
     }
 
@@ -117,7 +117,7 @@ contract RationalMathTest is Test {
         if (m == 0) m = 1;
         uint n = 18121667;
         uint d = 1002299;
-        Rational memory r = RationalMath.from(n * m, d * m);
+        rational memory r = Rational.from(n * m, d * m);
         r = r.reduce();
         assertEq(r.numerator, n);
         assertEq(r.denominator, d);
@@ -126,7 +126,7 @@ contract RationalMathTest is Test {
     function testReduce0(uint256 d) public {
         if (d == 0) d = 1;
         uint n = 0;
-        Rational memory r = RationalMath.from(n, d);
+        rational memory r = Rational.from(n, d);
         r = r.reduce();
         assertEq(r.numerator, 0);
         assertEq(r.denominator, 1);
@@ -134,7 +134,7 @@ contract RationalMathTest is Test {
 
     function testReduce1(uint256 nd) public {
         if (nd == 0) nd = 1;
-        Rational memory r = RationalMath.from(nd, nd);
+        rational memory r = Rational.from(nd, nd);
         r = r.reduce();
         assertEq(r.numerator, 1);
         assertEq(r.denominator, 1);
@@ -143,18 +143,18 @@ contract RationalMathTest is Test {
     // Add
 
     function testAddIntegerTo0(uint128 integer) public {
-        Rational memory a = RationalMath.from(0, 1);
-        Rational memory b = RationalMath.from(integer);
-        Rational memory r = a._add(b);
+        rational memory a = Rational.from(0, 1);
+        rational memory b = Rational.from(integer);
+        rational memory r = a._add(b);
         assertEq(r.numerator, integer);
         assertEq(r.denominator, a.denominator);
     }
 
     function testAddTo0(uint128 n, uint128 d) public {
         if (d == 0) d = 1;
-        Rational memory a = RationalMath.from(0, 1);
-        Rational memory b = RationalMath.from(n, d);
-        Rational memory r = a._add(b);
+        rational memory a = Rational.from(0, 1);
+        rational memory b = Rational.from(n, d);
+        rational memory r = a._add(b);
         assertEq(r.numerator, n);
         assertEq(r.denominator, d);
     }
@@ -162,13 +162,13 @@ contract RationalMathTest is Test {
     function testAddInteger(uint128 integer) public {
         uint n = 18121667;
         uint d = 1002299;
-        Rational memory a = RationalMath.from(n, d);
-        Rational memory b = RationalMath.from(integer);
-        Rational memory r = a._add(b);
+        rational memory a = Rational.from(n, d);
+        rational memory b = Rational.from(integer);
+        rational memory r = a._add(b);
         assertEq(r.numerator, n + integer * d);
         assertEq(r.denominator, a.denominator);
 
-        Rational memory r2 = b._add(a);
+        rational memory r2 = b._add(a);
         assertEq(r.numerator, r2.numerator);
         assertEq(r.denominator, r2.denominator);
     }
@@ -177,48 +177,48 @@ contract RationalMathTest is Test {
         if (d2 == 0) d2 = 1;
         uint n = 18121667;
         uint d = 1002299;
-        Rational memory a = RationalMath.from(n, d);
-        Rational memory b = RationalMath.from(n2, d2);
-        Rational memory r = a._add(b);
+        rational memory a = Rational.from(n, d);
+        rational memory b = Rational.from(n2, d2);
+        rational memory r = a._add(b);
         assertEq(r.numerator, n * d2 + d * n2);
         assertEq(r.denominator, d * d2);
 
-        Rational memory r2 = b._add(a);
+        rational memory r2 = b._add(a);
         assertEq(r.numerator, r2.numerator);
         assertEq(r.denominator, r2.denominator);
     }
 
     function testSubInteger(uint128 integer) public {
-        Rational memory a = RationalMath.from(type(uint128).max);
-        Rational memory b = RationalMath.from(integer);
-        Rational memory r = a._sub(b);
+        rational memory a = Rational.from(type(uint128).max);
+        rational memory b = Rational.from(integer);
+        rational memory r = a._sub(b);
         assertEq(r.numerator, uint(type(uint128).max) - integer);
         assertEq(r.denominator, 1);
     }
 
     function testSub(uint128 n, uint128 d) public {
         if (d == 0) d = 1;
-        Rational memory a = RationalMath.from(type(uint128).max);
-        Rational memory b = RationalMath.from(n, d);
-        Rational memory r = a._sub(b);
+        rational memory a = Rational.from(type(uint128).max);
+        rational memory b = Rational.from(n, d);
+        rational memory r = a._sub(b);
         assertEq(r.numerator, uint(type(uint128).max) * uint(d) - n);
         assertEq(r.denominator, d);
     }
 
     function testAddSub(uint64 i, uint64 n, uint64 d) public {
         if (d == 0) d = 1;
-        Rational memory a = RationalMath.from(i);
-        Rational memory b = RationalMath.from(n, d);
-        Rational memory r = a._add(b)._sub(b).reduce();
+        rational memory a = Rational.from(i);
+        rational memory b = Rational.from(n, d);
+        rational memory r = a._add(b)._sub(b).reduce();
         assertEq(r.numerator, a.numerator);
         assertEq(r.denominator, a.denominator);
     }
 
     function testSubAdd(uint64 i, uint64 n, uint64 d) public {
         if (d == 0) d = 1;
-        Rational memory a = RationalMath.from(type(uint128).max - i);
-        Rational memory b = RationalMath.from(n, d);
-        Rational memory r = a._sub(b)._add(b).reduce();
+        rational memory a = Rational.from(type(uint128).max - i);
+        rational memory b = Rational.from(n, d);
+        rational memory r = a._sub(b)._add(b).reduce();
         assertEq(r.numerator, a.numerator);
         assertEq(r.denominator, a.denominator);
     }
@@ -226,9 +226,9 @@ contract RationalMathTest is Test {
     function testMulInteger(uint128 integer) public {
         uint n = 18121667;
         uint d = 1002299;
-        Rational memory a = RationalMath.from(n, d);
-        Rational memory b = RationalMath.from(integer);
-        Rational memory r = a._mul(b);
+        rational memory a = Rational.from(n, d);
+        rational memory b = Rational.from(integer);
+        rational memory r = a._mul(b);
         assertEq(r.numerator, n * integer);
         assertEq(r.denominator, d);
     }
@@ -237,9 +237,9 @@ contract RationalMathTest is Test {
         if (d2 == 0) d2 = 1;
         uint n = 18121667;
         uint d = 1002299;
-        Rational memory a = RationalMath.from(n, d);
-        Rational memory b = RationalMath.from(n2, d2);
-        Rational memory r = a._mul(b);
+        rational memory a = Rational.from(n, d);
+        rational memory b = Rational.from(n2, d2);
+        rational memory r = a._mul(b);
         assertEq(r.numerator, n * n2);
         assertEq(r.denominator, d * d2);
     }
@@ -247,9 +247,9 @@ contract RationalMathTest is Test {
     function testMul2Sub(uint64 n, uint64 d) public {
         if (n == 0) n = 1;
         if (d == 0) d = 1;
-        Rational memory a = RationalMath.from(n, d).reduce();
-        Rational memory _2 = RationalMath.from(2);
-        Rational memory r = a._mul(_2)._sub(a).reduce();
+        rational memory a = Rational.from(n, d).reduce();
+        rational memory _2 = Rational.from(2);
+        rational memory r = a._mul(_2)._sub(a).reduce();
         assertEq(r.numerator, a.numerator);
         assertEq(r.denominator, a.denominator);
     }
@@ -258,9 +258,9 @@ contract RationalMathTest is Test {
     function testDivInteger(uint128 integer) public {
         uint n = 18121667;
         uint d = 1002299;
-        Rational memory a = RationalMath.from(n, d);
-        Rational memory b = RationalMath.from(integer);
-        Rational memory r = a._div(b);
+        rational memory a = Rational.from(n, d);
+        rational memory b = Rational.from(integer);
+        rational memory r = a._div(b);
         assertEq(r.numerator, n);
         assertEq(r.denominator, d * integer);
     }
@@ -270,70 +270,69 @@ contract RationalMathTest is Test {
         if (d2 == 0) d2 = 1;
         uint n = 18121667;
         uint d = 1002299;
-        Rational memory a = RationalMath.from(n, d);
-        Rational memory b = RationalMath.from(n2, d2);
-        Rational memory r = a._div(b);
+        rational memory a = Rational.from(n, d);
+        rational memory b = Rational.from(n2, d2);
+        rational memory r = a._div(b);
         assertEq(r.numerator, n * d2);
         assertEq(r.denominator, d * n2);
     }
 
-
     function testAddDiv2(uint64 n, uint64 d) public {
         if (n == 0) n = 1;
         if (d == 0) d = 1;
-        Rational memory a = RationalMath.from(n, d).reduce();
-        Rational memory _2 = RationalMath.from(2);
-        Rational memory r = a._add(a)._div(_2).reduce();
+        rational memory a = Rational.from(n, d).reduce();
+        rational memory _2 = Rational.from(2);
+        rational memory r = a._add(a)._div(_2).reduce();
         assertEq(r.numerator, a.numerator);
         assertEq(r.denominator, a.denominator);
     }
 
     // Downgrade
 
-    function testMostSignificantBitMax256() public {
-        uint bit = RationalMath.mostSignificantBit(type(uint256).max);
+    function testSignificantBitsMax256() public {
+        uint bit = Rational.significantBits(type(uint256).max);
         assertEq(bit, 256);
     }
 
-    function testMostSignificantBitMax128() public {
-        uint bit = RationalMath.mostSignificantBit(type(uint128).max);
+    function testSignificantBitsMax128() public {
+        uint bit = Rational.significantBits(type(uint128).max);
         assertEq(bit, 128);
     }
 
-    function testMostSignificantBitMax64() public {
-        uint bit = RationalMath.mostSignificantBit(type(uint64).max);
+    function testSignificantBitsMax64() public {
+        uint bit = Rational.significantBits(type(uint64).max);
         assertEq(bit, 64);
     }
 
-    function testMostSignificantBitMax32() public {
-        uint bit = RationalMath.mostSignificantBit(type(uint32).max);
+    function testSignificantBitsMax32() public {
+        uint bit = Rational.significantBits(type(uint32).max);
         assertEq(bit, 32);
     }
 
-    function testMostSignificantBitMax16() public {
-        uint bit = RationalMath.mostSignificantBit(type(uint16).max);
+    function testSignificantBitsMax16() public {
+        uint bit = Rational.significantBits(type(uint16).max);
         assertEq(bit, 16);
     }
 
-    function testMostSignificantBitMax8() public {
-        uint bit = RationalMath.mostSignificantBit(type(uint8).max);
+    function testSignificantBitsMax8() public {
+        uint bit = Rational.significantBits(type(uint8).max);
         assertEq(bit, 8);
     }
 
-    function testMostSignificantBit1() public {
-        uint bit = RationalMath.mostSignificantBit(1);
+    function testSignificantBits1() public {
+        uint bit = Rational.significantBits(1);
         assertEq(bit, 1);
     }
 
-    function testMostSignificantBit0() public {
-        uint bit = RationalMath.mostSignificantBit(0);
+    function testSignificantBits0() public {
+        uint bit = Rational.significantBits(0);
         assertEq(bit, 0);
     }
 
-    function testMostSignificantBit(uint8 b) public {
+    function testSignificantBits(uint8 b) public {
         uint num = 1 << b;
-        uint bit = RationalMath.mostSignificantBit(num);
-        assertEq(bit, 0);
+        uint bit = Rational.significantBits(num);
+        assertEq(bit, uint(b) + 1);
     }
 
 }
